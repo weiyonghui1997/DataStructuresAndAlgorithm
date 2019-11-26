197a6e08d2b3f06ab716248f4a9db3f8eff5b398
weiyonghui1997 <58197510+weiyonghui1997@users.noreply.github.com>
2019-11-26 11:45:17 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _789
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
